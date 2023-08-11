cf53069db22d7d97dd314c948d89875dd51baaf9
akakream <akakream@gmail.com>
2023-08-11 23:24:07 +0200

    mypy venv problem

--- after/plugin/nulls.lua
+++ after/plugin/nulls.lua
@@ -7,7 +7,12 @@ null_ls.setup({
         null_ls.builtins.formatting.goimports_reviser,
         null_ls.builtins.formatting.golines,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.mypy.with({
+            extra_args = function()
+            local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_DEFAULT_ENV") or "/usr"
+            return { "--python-executable", virtual .. "/bin/python3" }
+            end,
+        }),
         null_ls.builtins.diagnostics.ruff,
     },
     on_attach = function(client, bufnr)

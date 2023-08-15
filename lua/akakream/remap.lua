ae262b02a83e158a963be3b7ccf93d570e6c98c9
Ahmet Kerem Aksoy <kerem.aksoy@heyjobs.de>
2023-08-15 11:41:27 +0200

    resize nvimtree

--- lua/akakream/remap.lua
+++ lua/akakream/remap.lua
@@ -48,3 +48,6 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>bt", "<cmd>NvimTreeResize 80<CR>");
+vim.keymap.set("n", "<leader>nt", "<cmd>NvimTreeResize 30<CR>");

bc110283eaa5d25267063dc1973dcb2deb9aaa26
akakream <akakream@gmail.com>
2023-08-17 15:29:59 +0200

    ignore vendor also for grepping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,11 +1,27 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', function()
-    builtin.find_files({
-        file_ignore_patterns = {"vendor"},
-    })
-end, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+vim.keymap.set('n', '<leader>pf',
+    function()
+        builtin.find_files({
+            file_ignore_patterns = {"vendor"},
+        })
+    end,
+{})
+
+vim.keymap.set('n', '<C-p>',
+    function()
+        builtin.git_files({
+            file_ignore_patterns = {"vendor"},
+        })
+    end,
+{})
+
+vim.keymap.set('n', '<leader>ps',
+    function()
+        builtin.grep_string({
+            search = vim.fn.input("Grep > "),
+            file_ignore_patterns = {"vendor"},
+        })
+    end
+)
+
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
